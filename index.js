0bf4d76bd72e94c7166a91a1fe11b542b2fe2a5c
Preet Singh <preet10syd@gmail.com>
2024-09-08 15:20:56 +1000

    add operator functions and only plus operator is working

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-let output = 0;
+let output = '';
+let operator = '';
+let num1;
+let num2;
+
+// operator functions
+
+function add(num1,num2){
+    return num1 + num2
+}
+function subtraction(num1,num2){
+    return num1 - num2
+}
+function multiply(num1,num2){
+    return num1 * num2
+}
+function divide(num1,num2){
+    return num1 / num2
+}
 
 // display
 const display = document.querySelector(".display");
@@ -20,6 +38,83 @@ const zero = document.querySelector(".zero-button");
 // event listener for numeric buttons
 
 one.addEventListener("click", () => {
-  output = 1;
+  output += "1";
   display.textContent = output;
 });
+
+
+two.addEventListener("click", () => {
+  output += "2";
+  display.textContent = output;
+});
+three.addEventListener("click", () => {
+  output += "3";
+  display.textContent = output;
+});
+
+four.addEventListener("click", () => {
+  output += "4";
+  display.textContent = output;
+});
+five.addEventListener("click", () => {
+  output += "5";
+  display.textContent = output;
+});
+
+six.addEventListener("click", () => {
+  output += "6";
+  display.textContent = output;
+});
+seven.addEventListener("click", () => {
+  output += "7";
+  display.textContent = output;
+});
+
+eight.addEventListener("click", () => {
+  output += "8";
+  display.textContent = output;
+});
+nine.addEventListener("click", () => {
+  output += "9";
+  display.textContent = output;
+});
+
+zero.addEventListener("click", () => {
+  output += "0";
+  display.textContent = output;
+});
+
+// clear-button node
+const clear = document.querySelector(".clear-button");
+// clear button event listener
+clear.addEventListener('click',()=>{
+    output = '';
+    display.textContent = output;
+})
+
+// operator buttons
+
+const plus = document.querySelector('.plus-button');
+plus.addEventListener('click',()=>{
+    operator = '+';
+    num1 = +output;
+    output = '';
+
+})
+
+// equal button
+
+const equal = document.querySelector('.equal-button');
+equal.addEventListener('click',()=>{
+    if(operator === '+'){
+        display.textContent = '';
+        num2 = +output;
+        display.textContent = add(num1,num2);
+        output = '';
+
+        
+
+    }
+})
+
+

864e91b3394947f5b28fe652f27bea6e7d37bbb4
Preet Singh <preet10syd@gmail.com>
2024-09-14 14:18:22 +1000

    Fix continous calculation bug

--- index.js
+++ index.js
@@ -1,37 +1,72 @@
 let output = "";
-let operator = "";
-let calculatedResult;
-let num1;
-let num2;
+let operator = null;
+let calculatedResult = null;
+let num1 = null;
+let num2 = null;
+let decimalClicked = false;
+let minusClikced = false;
 
-// operator functions
+// calculate function
 
-function add(num1, num2) {
-  return num1 + num2;
-}
-function subtraction(num1, num2) {
-  return num1 - num2;
-}
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-function divide(num1, num2) {
-  return num1 / num2;
-}
-function percentage(num1, num2) {
-  return (num1 / 100) * num2;
+function calculate(num1, num2, operator) {
+  if (num1 && num2 && operator !== null) {
+    switch (operator) {
+      case "+":
+        calculatedResult = num1 + num2;
+        break;
+      case "-":
+        calculatedResult = num1 - num2;
+        break;
+      case "*":
+        calculatedResult = num1 * num2;
+        break;
+      case "/":
+        if(num2 !== 0){
+          calculatedResult = num1 / num2;
+        }else{
+          calculatedResult = 0;
+          display.textContent = 'Seriouslyy 😑';
+        }
+ 
+        break;
+      case "%":
+        calculatedResult = (num1 / 100) * num2;
+        break;
+    }
+  }
 }
 
 // display
 const display = document.querySelector(".display");
 display.textContent = 0;
 
-// getting all the numeric button nodes
-
+// decimal
 const decimal = document.querySelector(".decimal-button");
-const minusValue = document.querySelector(".minus-value-button");
+decimal.addEventListener("click", () => {
+  if (output === "") {
+    output += "0.";
+    decimalClicked = true;
+  } else if (!decimalClicked) {
+    output += ".";
+    decimalClicked = true;
+  }
 
-// using queryselectoall
+  display.textContent = output;
+});
+
+// minus-button
+const minusValue = document.querySelector(".minus-value-button");
+minusValue.addEventListener("click", () => {
+
+if(minusClikced === false && output !== ''){
+    output = "-" + output;
+    minusClikced = true;
+    display.textContent = output;
+  }
+ 
+});
+
+// using queryselectoall to get all number buttons
 const numericButtons = document.querySelectorAll(".numeric-button");
 numericButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -40,106 +75,63 @@ numericButtons.forEach((button) => {
   });
 });
 
-decimal.addEventListener("click", () => {
-  output += ".";
-  display.textContent = output;
-});
-minusValue.addEventListener("click", () => {
-  output = "-" + output;
-  display.textContent = output;
-});
-
 // clear-button
 const clear = document.querySelector(".clear-button");
 clear.addEventListener("click", () => {
   output = "";
-  num1 = "";
-  num2 = "";
+  num1 = null;
+  num2 = null;
+  calculatedResult = null;
   display.textContent = 0;
 });
 
 // operator buttons
+const operatorButtons = document.querySelectorAll(".operator-buttons");
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    operator = button.textContent;
 
-// plus button
-const plusButton = document.querySelector(".plus-button");
-plusButton.addEventListener("click", () => {
-  operator = "+";
-  num1 = Number(output);
-  output = "";
-});
-// minus button
-const minusButton = document.querySelector(".minus-button");
-minusButton.addEventListener("click", () => {
-  operator = "-";
-  num1 = Number(output);
-  output = "";
-});
-// multiply button
-const multiplyButton = document.querySelector(".multiply-button");
-multiplyButton.addEventListener("click", () => {
-  operator = "*";
-  num1 = +output;
-  output = "";
-});
-// divide button
-const divideButton = document.querySelector(".divide-button");
-divideButton.addEventListener("click", () => {
-  operator = "/";
-  num1 = +output;
-  output = "";
-});
-// percentage button
-const percentageButton = document.querySelector(".percentage-button");
-percentageButton.addEventListener("click", () => {
-  operator = "%";
-  num1 = +output;
-  output = "";
+    minusClikced = false;
+    decimalClicked = false;
+
+    if (num1 === null) {
+      num1 = Number(output);
+      output = "";
+    } else {
+      if (calculatedResult !== null) {
+        num1 = calculatedResult;
+        num2 = Number(output);
+        calculate(num1, num2, operator);
+        display.textContent = calculatedResult;
+        num1 = calculatedResult;
+        output = "";
+      } else {
+        num2 = Number(output);
+        calculate(num1, num2, operator);
+        display.textContent = calculatedResult;
+        num1 = calculatedResult;
+        output = "";
+      }
+    }
+    console.log("operator: ", operator);
+    console.log("calculated result: ", calculatedResult);
+
+    console.log("num1:", num1);
+    console.log("num2: ", num2);
+  });
 });
 
 // equal button
 
 const equal = document.querySelector(".equal-button");
 equal.addEventListener("click", () => {
-  if (operator === "+") {
-    display.textContent = "";
-    num2 = Number(output);
-    calculatedResult = add(num1, num2);
-    display.textContent = calculatedResult;
-    num1 = calculatedResult;
-
-    output = "";
-  } else if (operator === "-") {
-    display.textContent = "";
-    num2 = Number(output);
-
-    calculatedResult = subtraction(num1, num2);
-    display.textContent = calculatedResult;
-    num1 = calculatedResult;
-
-    output = "";
-  } else if (operator === "*") {
-    display.textContent = "";
-    num2 = Number(output);
-    calculatedResult = multiply(num1, num2);
-    display.textContent = calculatedResult;
-    num1 = calculatedResult;
-
-    output = "";
-  } else if (operator === "/") {
-    display.textContent = "";
-    num2 = Number(output);
-    calculatedResult = divide(num1, num2);
-    display.textContent = calculatedResult;
-    num1 = calculatedResult;
-
-    output = "";
-  } else if (operator === "%") {
-    display.textContent = "";
-    num2 = Number(output);
-    calculatedResult = percentage(num1, num2);
-    display.textContent = calculatedResult;
-    num1 = calculatedResult;
-
-    output = "";
-  }
+ 
+  minusClikced = false;
+  decimalClicked = false;
+ 
+  num2 = Number(output);
+  calculate(num1, num2, operator);
+  display.textContent = calculatedResult;
+  num1 = calculatedResult;
+  output = "";
 });
